9de01288a35a131676d19e92cd7ef8874e8c5c11
Youngho228 <129716276+Youngho228@users.noreply.github.com>
2023-04-04 10:09:35 +0900

    commit

---
+++ customer/src/main/java/fooddelivery/domain/OrderPlaced.java
@@ -0,0 +1,25 @@
+package fooddelivery.domain;
+
+import fooddelivery.domain.*;
+import fooddelivery.infra.AbstractEvent;
+import java.util.*;
+import lombok.*;
+
+@Data
+@ToString
+public class OrderPlaced extends AbstractEvent {
+
+    private Long id;
+    private String customerid;
+    private String foodid;
+    private String option;
+    private String address;
+
+    public OrderPlaced(Order aggregate) {
+        super(aggregate);
+    }
+
+    public OrderPlaced() {
+        super();
+    }
+}
